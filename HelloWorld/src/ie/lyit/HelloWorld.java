9ec39d77d66d5223c78a475d795616e58f9e28bc
97047619 <97047619@student.lyit.ie>
2021-02-14 20:42:38 +0000

    First project commit

---
+++ HelloWorld/src/ie/lyit/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ie.lyit;
+
+/**
+ * @author mconaghan
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World on lubuntu VMware");
+
+	}
+
+}
---
+++ HelloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mconaghan
+ *
+ */
+module HelloWorld {
+}
